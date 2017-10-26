96274828fb931d7a4e0fd93d58a541490c75c5ea
zappybiby <blittle65@gmail.com>
2017-10-26 11:54:52 -0400

    Trying to seperate grabwindow
    Might not work like this.

---
+++ grabwindow.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import win32gui
+import win32ui
+from ctypes import windll
+
+hwnd = win32gui.FindWindow("UnityWndClass", None)
+
+def grab_window():
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w = right - left
+    h = bot - top
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+    saveDC.SelectObject(saveBitMap)
+
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 0)
+    print(result)
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im = Image.frombuffer(
+        'RGB',
+        (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+        bmpstr, 'raw', 'BGRX', 0, 1)
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    if result == 1:
+        # PrintWindow Succeeded
+        im.save("test.png")
+        print("Game Screenshot Saved")
+
+if __name__ == '__main__':
+    grab_window()
--- main.py
+++ main.py
@@ -5,43 +5,6 @@ from PIL import Image
 import win32gui
 import win32ui
 from ctypes import windll
+from grabwindow import grab_window
 
-hwnd = win32gui.FindWindow("UnityWndClass", None)
-
-left, top, right, bot = win32gui.GetWindowRect(hwnd)
-w = right - left
-h = bot - top
-
-hwndDC = win32gui.GetWindowDC(hwnd)
-mfcDC = win32ui.CreateDCFromHandle(hwndDC)
-saveDC = mfcDC.CreateCompatibleDC()
-
-saveBitMap = win32ui.CreateBitmap()
-saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
-
-saveDC.SelectObject(saveBitMap)
-
-result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 0)
-print(result)
-
-bmpinfo = saveBitMap.GetInfo()
-bmpstr = saveBitMap.GetBitmapBits(True)
-
-im = Image.frombuffer(
-    'RGB',
-    (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-    bmpstr, 'raw', 'BGRX', 0, 1)
-
-win32gui.DeleteObject(saveBitMap.GetHandle())
-saveDC.DeleteDC()
-mfcDC.DeleteDC()
-win32gui.ReleaseDC(hwnd, hwndDC)
-
-if result == 1:
-    # PrintWindow Succeeded
-    im.save("test.png")
-    print("Game Screenshot Saved")
-
-# Convert to numpy array
-im2 = cv2.imread("test.png")
-print(type(im2))
+grab_window()
