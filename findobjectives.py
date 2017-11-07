fcb3cec8cabbdd4e9775ca50ab8441f27ac79256
Kirin Patel <kirinpatel@gmail.com>
2017-11-06 20:32:54 -0500

    Fixed #3

--- findobjectives.py
+++ findobjectives.py
@@ -11,15 +11,22 @@ def analyze_window(window, window_x, window_y):
 
 def locate_nav(window):
     print('locating nav')
-    lower = np.array([15, 90, 50], np.uint8)
-    upper = np.array([30, 255, 255], np.uint8)
+
+    lower = np.array([15, 95, 55], np.uint8)
+    upper = np.array([25, 255, 255], np.uint8)
     img = cv2.inRange(cv2.cvtColor(window, cv2.COLOR_BGR2HSV), lower, upper)
-    template = cv2.imread('res/nav.jpg', 0)
+    dim = (100, 100)
+    template = cv2.resize(cv2.imread('res/nav.jpg', 0), dim, interpolation=cv2.INTER_AREA)
+
+    mask = np.zeros(img.shape, dtype="uint8")
+    cv2.rectangle(mask, (0, 0), (1280, 860), (255, 255, 255), -1)
+    cv2.rectangle(mask, (1160, 35), (1200, 65), (0, 0, 0), -1)
+
+    blur = cv2.medianBlur(cv2.bitwise_and(img, mask), 5)
+
     w, h = template.shape[::-1]
-    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
+    res = cv2.matchTemplate(blur, template, cv2.TM_CCOEFF)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    if min_val > -74000000 and max_val > 14000000:
-        print(str(min_loc[0]) + " : " + str(min_loc[1]))
-        bottom_right = (min_loc[0] + w, min_loc[1] + h)
-        cv2.rectangle(img, min_loc, bottom_right, 255, 1)
-    cv2.imshow('BomberCrewBot 2', img)
+    bottom_right = (min_loc[0] + w, min_loc[1] + h)
+    cv2.rectangle(blur, min_loc, bottom_right, 255, 1)
+    cv2.imshow('BomberCrewBot 2', blur)
