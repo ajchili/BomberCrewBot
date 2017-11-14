0dc2341dca420674d645cd591c1df7553e8fa0f2
zappybiby <blittle65@gmail.com>
2017-11-14 10:20:55 -0500

    Create setup.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from grabwindow import grab_window
 from crewmembers import get_active_crew_members
-import cv2
-import numpy as np
 
 crew_members = get_active_crew_members()
 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='BomberCrewBot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='zappybiby',
+    author_email='',
+    description='', requires=['opencv-contrib-python', 'numpy', 'pyautogui', 'mss', 'pywin32', ]
+)
