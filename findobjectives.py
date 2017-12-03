ef50d95e69b909ccbf0a5b6b0a152d75e3b7b0b2
Kirin Patel <kirinpatel@gmail.com>
2017-12-02 23:51:28 -0500

    Removed nav.png and fixed formatting

--- findobjectives.py
+++ findobjectives.py
@@ -31,7 +31,7 @@ def locate_nav(window, width, height, is_day, window_x, window_y):
         upper = NAV_NIGHT_UPPER
 
     img = cv2.inRange(cv2.cvtColor(window, cv2.COLOR_BGR2HSV), lower, upper)
-    template = cv2.imread('res/map_50px.png', 0)
+    template = cv2.imread('res/nav.png', 0)
     blur = cv2.medianBlur(cv2.bitwise_and(img, img), 5)
 
     w, h = template.shape[::-1]
@@ -41,7 +41,7 @@ def locate_nav(window, width, height, is_day, window_x, window_y):
 
     if min_val < -500000 and max_val > 600000:
         if use_mouse:
-            mouse_nav.move(width, height,min_loc[0] + (w / 2), min_loc[1] + (h / 2))
+            mouse_nav.move(width, height, min_loc[0] + (w / 2), min_loc[1] + (h / 2))
         else:
             threading.Thread(target=keyboard_nav.move, args=(width, height, min_loc[0] + (w / 2), min_loc[1] + (h / 2),)).start()
         cv2.rectangle(blur, min_loc, bottom_right, 255, 1)
